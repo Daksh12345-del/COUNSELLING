618a44aea60ab6a30eafd8f367af6dcb5a08740f
Daksh Singhal <psinghal651@gmail.com>
2026-03-22 20:42:06 +0530

    Fix site

---
+++ main.js
@@ -0,0 +1,9 @@
+// Sarvpratham Consultancy – main.js
+
+function scrollToEnquiry() {
+  document.getElementById('enquiry').scrollIntoView({ behavior: 'smooth' });
+}
+
+function submitEnquiry() {
+  alert('Thank you! We will contact you within 24 hours.');
+}
